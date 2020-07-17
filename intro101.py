742f029d5e559fe41f9102997d719e38bf6c33e4
Okechukwu Okigbo <okigbookey@gmail.com>
2020-07-17 11:20:16 -0400

    Update intro101.py

--- intro101.py
+++ intro101.py
@@ -8,3 +8,11 @@ a = tch.tensor([23,56 ,78],[90, 12,31])  # a = np.array([23,56 ,78],[90, 12,31])
 b = tch.rand(3,2)  # b = np.random.randn(3,2)
 
 print2(a.shape, tch.Size(a))  # print2(a.shape, a.size())
+
+
+# Matrix multiplication
+c = a.matmul(b)   # c = a.dot(b)
+
+# Element by element multiplication
+
+d = a * b   # d = a.multiply(b)
