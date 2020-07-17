3ce2ad64413d077ee48ab38187d72dfce9029e2e
Okechukwu Okigbo <okigbookey@gmail.com>
2020-07-17 11:18:20 -0400

    Update intro101.py

--- intro101.py
+++ intro101.py
@@ -1,9 +1,10 @@
+import numpy as np
 import torch as tch
 from printdescribe import print2, describe2, changepath
 
 
-a = tch.tensor([23,56 ,78],[90, 12,31])
+a = tch.tensor([23,56 ,78],[90, 12,31])  # a = np.array([23,56 ,78],[90, 12,31]) 
 
-b = tch.rand(3,2)
+b = tch.rand(3,2)  # b = np.random.randn(3,2)
 
-print2(a.shape, tch.Size(a))
+print2(a.shape, tch.Size(a))  # print2(a.shape, a.size())
