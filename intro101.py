748116272d11f81bc558648881a2e77eec9e04b9
jocoder22 <okigbookey@gmail.com>
2020-07-19 21:35:26 -0400

    Update

--- intro101.py
+++ intro101.py
@@ -3,11 +3,11 @@ import torch as tch
 from printdescribe import print2, describe2, changepath
 
 
-a = tch.tensor([23,56 ,78],[90, 12,31])  # a = np.array([23,56 ,78],[90, 12,31]) 
+a = tch.tensor([[23,56 ,78],[90, 12,31]])  # a = np.array([23,56 ,78],[90, 12,31]) 
 
 b = tch.rand(3,2)  # b = np.random.randn(3,2)
 
-print2(a.shape, tch.size())  # print2(a.shape, a.size())
+print2(a.shape)  # print2(a.shape, a.size())
 
 
 # Matrix multiplication
