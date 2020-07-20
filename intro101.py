bed97bd6eb137acdd2be8c5561052566b4024fab
jocoder22 <okigbookey@gmail.com>
2020-07-19 22:29:30 -0400

    Update

--- intro101.py
+++ intro101.py
@@ -1,40 +1,44 @@
 import numpy as np
-import torch as tch
+import torch
 from printdescribe import print2, describe2, changepath
 
 
-a = tch.tensor([[23,56 ,78],[90, 12,31]])  # a = np.array([23,56 ,78],[90, 12,31]) 
+a = torch.tensor([[23,56 ,78],[90, 12,31]])  # a = np.array([23,56 ,78],[90, 12,31]) 
 
-b = tch.rand((3,2))  # b = np.random.randn(3,2)
+b = torch.rand((3,2))  # b = np.random.randn(3,2)
 
 print2(a.shape)  # print2(a.shape, a.size())
 
-
 # Matrix multiplication
-c = tch.matmul(a, b)   # c = a.dot(b)
+a = torch.rand((3, 3))
+b = torch.rand((3, 3))
+
+c = torch.matmul(a, b)   # c = a.dot(b)
 
 # Element by element multiplication
-
 d = a * b   # d = np.multiply(a, b)
 
+print2(c, d)
 
 # Zeroes, ones and identity matrix
-z = tch.zeros(4, 5)  # z = np.zeros(4, 5)
-oo = tch.ones(2, 3)  # oo = np.ones(2, 3)
-idtt = tch.eye(4)   # idtt = np.identity(4)
+z = torch.zeros(4, 5)  # z = np.zeros(4, 5)
+oo = torch.ones(5, 6)  # oo = np.ones(2, 3)
+idtt = torch.eye(4)   # idtt = np.identity(4)
+
+print2(z, oo, idtt)
 
 # torch-numpy interchange
-t = tch.tensor([[4,5,7],[2,9,6]])
-n = np.array([2,3,9],[1,4,6])
+t = torch.tensor([[4,5,7],[2,9,6]])
+n = np.array([[2,3,9],[1,4,6]])
 
-torch_from_numpy = tch.from_numpy(n)
+torch_from_numpy = torch.from_numpy(n)
 numpy_from_torch = t.numpy()
 
 
 # Initialize x, y and z to values 4, -3 and 5
-x = tch.tensor(90., requires_grad=True)
-y = tch.tensor(-51., requires_grad=True)
-z = tch.tensor(12., requires_grad=True)
+x = torch.tensor(90., requires_grad=True)
+y = torch.tensor(-51., requires_grad=True)
+z = torch.tensor(12., requires_grad=True)
 
 # Set q to sum of x and y, set f to product of q with z
 t = x + y
