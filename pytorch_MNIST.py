467c9060b5dd4458f24505f9ffd31930921b4a75
Okechukwu Okigbo <okigbookey@gmail.com>
2020-07-28 15:23:24 -0400

    Update pytorch_MNIST.py

--- pytorch_MNIST.py
+++ pytorch_MNIST.py
@@ -1,2 +1,11 @@
-%matplotlab inline
-%config InlineBackend.fig_format = "retina"
+#!/usr/bin/env python
+# %matplotlab inline
+# %config InlineBackend.fig_format = "retina"
+
+import numpy as np
+import torch
+import matplotlab.pyplot as plt
+import helper
+
+from printdescribe import print2, describe2, changepath
+from .ml1o1 import sigmoid_activation
