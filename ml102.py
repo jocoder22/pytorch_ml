f498974b2792db7de45a6977de707aed1aa754ed
Okechukwu Okigbo <okigbookey@gmail.com>
2020-07-28 12:46:40 -0400

    Update ml102.py

--- ml102.py
+++ ml102.py
@@ -1,1 +1,32 @@
+#!/usr/bin/env python
+import torch
+from printdescribe import print2, describe2, changepath
+from .ml1o1 import sigmoid_activation
+
+
+# set the seed
+torch.manual_seed(90)
+
+# create features
+features = torch.randn((1,10))
+
+# define sizes of layers
+input_size = features.shape[1]
+n_hiddenlayers = 4
+n_output = 1
+
+# create weights
+feature_weights = torch.randn((input_size, n_hiddenlayers))
+hiddenlayer_weights = torch.randn((n_hiddenlayers, n_output))
+
+# create biases
+feature_bias = torch.randn((1, n_hiddenlayers))
+hiddenlayer_bias = torch.randn((1, n_output))
+
+
+## y = f2(f1(xW1)W2)
+
+hiddenlayer_output = sigmoid_activation(torch.mm(features,features_weights) + 
+                                          features_bias))
+y = sigmoid_activation(torch.mm(hiddenlayer_output, hiddenlayer_bias) + hiddenlayer_bias)
 
