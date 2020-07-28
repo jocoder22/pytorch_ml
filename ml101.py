bdb8a49f3452b732bd4c281dde14716f44a75ed3
Okechukwu Okigbo <okigbookey@gmail.com>
2020-07-28 10:50:52 -0400

    Update ml101.py

--- ml101.py
+++ ml101.py
@@ -1,4 +1,22 @@
 #!/usr/bin/env python
-import numpy as np
 import torch
 from printdescribe import print2, describe2, changepath
+
+
+# set the seed
+torch.manual_seed(90)
+
+def sigmoid_activation(x):
+  """ The Sigmoid activation function that result probabilities
+    
+    Input:
+      x: torch.Tensor
+      
+     Output:
+       y: float
+  
+  """
+  
+  y = 1/(1 + torch.exp(-x))
+  
+  return y
