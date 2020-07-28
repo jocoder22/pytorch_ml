ac77512a15f98aabceb6461cab1be65ba25ba536
Okechukwu Okigbo <okigbookey@gmail.com>
2020-07-28 15:34:12 -0400

    Update pytorch_MNIST.py

--- pytorch_MNIST.py
+++ pytorch_MNIST.py
@@ -3,9 +3,25 @@
 # %config InlineBackend.fig_format = "retina"
 
 import numpy as np
-import torch
 import matplotlab.pyplot as plt
 import helper
+import torch
+from torchvision import datasets, transforms
 
 from printdescribe import print2, describe2, changepath
 from .ml1o1 import sigmoid_activation
+
+
+# transformer to transform and normalize
+transformer = transforms.Compose([Transforms.ToTensor(), 
+                                 transforms.normalize((0.5,0.5, 0.5), (0.5, 0.5, 0.5))])
+
+traindata = datasets.MNIST('MNIST_data/', download=True, train=True, transform=transfomer)
+traindownloader = torch.utils.data.DataLoader(traindata, batch_size=64, shuffle=True)
+                                  
+                                 
+
+                                  
+                
+                                   
+                           
