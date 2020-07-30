6a7d00156eb2fd679f1362ddcf2a6ec559ec6ee5
jocoder22 <okigbookey@gmail.com>
2020-07-30 18:41:21 -0400

    Add new file pytorchFunctions.py

---
+++ pytorchFunctions.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+import torch
+
+def sigmoid_activation(x):
+  """ The Sigmoid activation function that result probabilities
+    
+    Input:
+      x: torch.Tensor
+      
+     Output:
+       y: float
+  
+  """
+  
+  y = 1/(1 + torch.exp(-x))
+  
+  return y
+
+
+def softmax_activation(x):
+  """ The softmax_activation function
+  
+    Inputs:
+      X: torch.Tensor
+      
+    Output:
+      p: torch.Tensor (probabilities)
+      
+    Numpy Implementation:
+        deno = np.exp(x).sum(axis=1).reshape(-1, 1)
+        return np.divide(np.exp(x), deno)
+  
+  """
+  p = torch.exp(x)/torch.sum(torch.exp(x), dim=1).view(-1, 1)
+  
+  return p
